b15c3bfe6feb4b9dd4ff5d06c28699dba74c532c
Odigietony <odigietony.jr@gmail.com>
2020-01-01 03:25:17 +0100

    Adding catalog models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+from django.urls import reverse
+#blank and null attributes when equals to null signify optional fields
 
-# Create your models here.
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=20)
+
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book.')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    
+    
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book.')
+
+
+    # Foreign Key used because book can only have one language, but language can have multiple books
+    # Language as a string rather than object because it hasn't been declared yet in the file
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse("book-detail", args=[str(self.id)])
+    
+
+
+import uuid # Required for unique book instances
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    # LOAN_STATUS is not a field, we just created this key-value pair tuple in order to porpulate the status choices attribute.
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id} ({self.book.title})'
+
+
+class Author(models.Model):
+    """Model representing an author."""
+    firstname = models.CharField('First Name', max_length=20)
+    lastname = models.CharField('Last Name', max_length=20)
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_death = models.DateField('Died', blank=True, null=True) #blank and null attributes when equals to null signify optional fields
+
+    class Meta:
+        ordering = ['lastname', 'firstname']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.lastname}, {self.firstname}'
+
+
+
+class Language(models.Model):
+    """Model representing a Language category"""
+    name = models.CharField(max_length=20, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
