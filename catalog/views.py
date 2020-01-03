d5634b424a42ac6b51f05a413d7c486fd40bcfd7
Odigietony <odigietony.jr@gmail.com>
2020-01-04 00:57:07 +0100

    Landing page views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from catalog.models import Book, BookInstance, Author
+from catalog.models import Book, BookInstance, Author, Genre
 
 def index(request):
     """View function for home page of site."""
@@ -7,15 +7,20 @@ def index(request):
     num_books = Book.objects.all().count()
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    num_book_instance = BookInstance.objects.all().count()
+    num_book_instances = BookInstance.objects.all().count()
     # Available books (status = 'a')
     num_available_instance = BookInstance.objects.filter(status__exact='a').count()
+    num_genre = Genre.objects.filter(name__icontains='fiction').count()
+    num_books_with_the_in_title = Book.objects.filter(title__icontains='the').count()
 
     context = {
         'num_books': num_books,
         'num_authors': num_authors,
-        'num_book_instance': num_book_instance,
-        'num_available_instance': num_available_instance
+        'num_book_instances': num_book_instances,
+        'num_available_instance': num_available_instance,
+        'title': 'Home',
+        'num_genre': num_genre,
+        'num_books_with_the_in_title': num_books_with_the_in_title,
     }
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
