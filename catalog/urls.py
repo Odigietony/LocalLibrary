21340980fb46628c132d87120f01cf4014ccbc09
Odigietony <odigietony.jr@gmail.com>
2020-01-08 14:36:01 +0100

    Forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -16,3 +16,7 @@ urlpatterns += [
 urlpatterns +=[
     path('borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'), 
+]
